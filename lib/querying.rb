8b2dfe91fed69caa2839f66cf63597ca94b72bcc
surpreet.mundra <surpreet.mundra@gmail.com>
2018-10-10 01:43:36 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -34,7 +34,10 @@ def select_series_title_with_most_human_characters
   "SELECT series.title
   FROM series
   INNER JOIN characters
-  ON series.id = characters.series_id"
+  ON series.id = characters.series_id
+  ORDER BY 
+  GROUP BY series.title
+  "
 end
 
 def select_character_names_and_number_of_books_they_are_in
